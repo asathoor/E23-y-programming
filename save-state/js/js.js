02651fad7862b64715f1e85dfce799a6fc11b1aa
Per Thykjaer Jensen <per@multimusen.dk>
2023-10-10 13:05:13 +0200

    update

--- save-state/js/js.js
+++ save-state/js/js.js
@@ -3,3 +3,17 @@
  * purpose: Behaviors
  **/
 console.log('Success: JavaScript from js/js.js running!')
+
+sessionStorage.setItem("burger", "Vegan Green Monster")
+// sessionStorage.getItem("burger");
+
+// get from the session storage
+console.log( sessionStorage.getItem("burger") )
+result.innerHTML = sessionStorage.getItem("burger")
+
+/** Local storage = store in the browser */
+localStorage.setItem("myName", "Per")
+
+console.log( "From browser storage: " + localStorage.getItem("myName") )
+
+welcome.innerHTML += localStorage.getItem("myName")
